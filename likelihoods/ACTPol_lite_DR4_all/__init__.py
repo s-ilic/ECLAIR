37eeef40280bdf5e0a393450f4d20b2f01591107
Stephane Ilic <stephane.ilic@gmail.com>
2020-07-18 01:02:51 +0200

    Implemented ACTPol lite DR4 likelihood

---
+++ likelihoods/ACTPol_lite_DR4_all/__init__.py
@@ -0,0 +1,22 @@
+import pyactlike
+
+### Some important variables ###
+use_tt = True
+use_te = True
+use_ee = True
+tt_lmax = 5000
+bmin = 0
+like = pyactlike.ACTPowerSpectrumData(
+    use_tt=use_tt,
+    use_te=use_te,
+    use_ee=use_ee,
+    tt_lmax=tt_lmax,
+    bmin=bmin,
+)
+
+### ACTPol lite DR4 likelihood
+def get_loglike(class_input, likes_input, class_run):
+    dell_tt = class_run.lensed_cl()['tt'][2:tt_lmax+1] * 1e12 * class_run.T_cmb()**2.
+    dell_te = class_run.lensed_cl()['te'][2:tt_lmax+1] * 1e12 * class_run.T_cmb()**2.
+    dell_ee = class_run.lensed_cl()['ee'][2:tt_lmax+1] * 1e12 * class_run.T_cmb()**2.
+    return like.loglike(dell_tt, dell_te, dell_ee, likes_input['yp2'])
