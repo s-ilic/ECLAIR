c61a8da74fc105765385eb5f405235311f9a238a
Stephane Ilic <stephane.ilic@gmail.com>
2021-08-20 00:43:11 +0200

    Fixed Pantheon likelihood

--- likelihoods/Pantheon/__init__.py
+++ likelihoods/Pantheon/__init__.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import numexpr
 import numpy as np
 from scipy.linalg import cholesky, solve_triangular
 
@@ -9,16 +8,21 @@ path_to_data = os.path.dirname(os.path.realpath(sys.argv[0])) + '/likelihoods/Pa
 
 ### Function loading and reshaping covariance matrices
 def load_mat(name):
-    path = path_to_data + name
-    tmp = np.loadtxt(path)
+    tmp = np.loadtxt(name)
     return tmp[1:].reshape((int(tmp[0]), int(tmp[0])))
 
 ### Loading the covariance matrices of the measurements
-mag_covmat = load_mat(path_to_data + 'sys_full_long.dat')
+mag_covmat = load_mat(path_to_data + 'sys_full_long.txt')
 
 ### Loading the light curve parameters
 lc_params = np.genfromtxt(path_to_data + 'lcparam_full_long.txt', names=True)
 n_data = len(lc_params)
+
+# Computing the cov mat
+cov_mat = mag_covmat + np.diag(lc_params['dmb']**2)
+
+# Computing its Cholesky decomposition
+cov_mat = cholesky(cov_mat, lower=True, overwrite_a=True)
 
 ### The likelihood function
 def get_loglike(class_input, likes_input, class_run):
@@ -31,20 +35,10 @@ def get_loglike(class_input, likes_input, class_run):
     M = likes_input['M']
 
     # Computing the difference of data distance moduli - model distance moduli
-    diff_dist = lc_params['mb'] - M - dist_mod   
+    diff_dist = lc_params['mb'] - M - dist_mod
 
-    # Computing the cov mat with nuisance parameters using numexpr for rapidity
-    cov_mat = numexpr.evaluate(mag_covmat)
-        
-    # Add statistical errors to diagonal terms of the cov mat
-    cov_mat += mag_covmat + np.diag(lc_params['dmb']**2)
-  
-    # Whitening the diff_dist :
-    # 1)Computing the Cholesky decomposition
-    cov_mat = cholesky(cov_mat, lower=True, overwrite_a=True)
-    # 2)Solving the triangular system
+    # Solving the triangular system
     diff_dist  = solve_triangular(cov_mat, diff_dist, lower=True, check_finite=False)
 
     # Return log like
     return -0.5 * (diff_dist**2).sum()
-
