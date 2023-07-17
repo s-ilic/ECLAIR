bb781b784f11551a3dbb6819b5980001cb8ae545
Stephane Ilic <stephane.ilic@gmail.com>
2023-07-17 05:27:10 +0200

    Modified plotting script to reflect the changes in the parser

--- ECLAIR_plots.py
+++ ECLAIR_plots.py
@@ -5,11 +5,9 @@ from ECLAIR_parser import *
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser,RawTextHelpFormatter
 
-
-####################################################################################
-####################################################################################
-
-# Parsing input arguments
+###############################
+### Parsing input arguments ###
+###############################
 
 parser = ArgumentParser(
     description='Analyser for outputs from the ECLAIR suite.',
@@ -21,13 +19,6 @@ parser.add_argument(
     nargs=1,
     type=str,
     help='Path to chain file to be analyzed.',
-)
-parser.add_argument(
-    '-c',
-    '--copy',
-    metavar='DIR',
-    action='store',
-    help='Make a temporary copy of the chain in DIR before analyzing.'
 )
 parser.add_argument(
     '-k',
@@ -113,42 +104,16 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-####################################################################################
-####################################################################################
+################################
+### Checking input arguments ###
+################################
 
-# Checking input arguments
-
-if not os.path.isfile(args.file[0]): # Input chain path
+# Check nput chain path
+if not os.path.isfile(args.file[0]):
     raise ValueError("Chain %s does not exist." % args.file[0])
-else:
-    test = (
-        args.file[0].endswith('.h5')
-        & os.path.isfile(args.file[0][:-3] + '.lock')
-        & (args.copy is None)
-    )
-    if test:
-        raise ValueError(
-            "Chain %s is HDF5 and running, judging from the existence on the .lock "
-            "file. If you are sure that your chain is not running, delete the .lock "
-            "file and retry. Otherwise, use the -c option (creating a temp copy of "
-            "the chain) to prevent I/O errors that could compromise"
-            "your MCMC run." % args.file[0]
-        )
-    if args.file[0].endswith('.h5'):
-        ftype = 'HDF5'
-    elif args.file[0].endswith('.txt'):
-        ftype = 'text'
-    else:
-        raise ValueError("Unrecognized file format for input chain %s, should be either .txt or .h5" % args.file[0])
 
-if args.copy is not None: # Temp copy path
-    if not os.path.isdir(args.copy):
-        print("Copy path %s does not exist or is not a directory, defaulting to current directory." % args.copy)
-        cp_path = os.path.dirname(os.path.realpath(sys.argv[0]))
-    else:
-        cp_path = os.path.realpath(args.copy)
-
-keep = [] # Parameter list
+# List of parameters to keep
+keep = []
 if args.keep is not None:
     tmp = args.keep[0].split(',')
     for t in tmp:
@@ -166,7 +131,8 @@ if args.keep is not None:
             keep += range(int(st[0]), int(st[1]) + 1)
 keep = np.unique(keep)
 
-plot = [] # Plot lists
+# List of plots to produce
+plot = []
 if args.plot is not None:
     tmp = args.plot[0].split(',')
     tmp_plot = []
@@ -185,11 +151,11 @@ if args.plot is not None:
             tmp_plot += range(int(st[0]), int(st[1]) + 1)
     for p in tmp_plot:
         if p not in [1,2,3,4,5]:
-            print("Unrecognised plot option %s, ignored." % p)
-        else:
-            plot.append(p)
+            print("Unrecognised plot option %s, will be ignored." % p)
+        plot.append(p)
 
-if args.burn_in is None: # Burn-in
+# Burn-in settings
+if args.burn_in is None:
     burn_is_float = False
     burn = 0
 else:
@@ -204,7 +170,8 @@ else:
         burn_is_float = False
         burn = int(args.burn_in[0])
 
-if args.thin is None: # Sample thinning
+# Thinning settings
+if args.thin is None:
     thin = 1
 else:
     if not is_number(args.thin[0]):
@@ -214,7 +181,8 @@ else:
     else:
         thin = int(args.thin[0])
 
-if args.thin_walk is None: # Walker thinning
+# Walker thinning settings
+if args.thin_walk is None:
     thinw = 1
 else:
     if not is_number(args.thin_walk[0]):
@@ -224,6 +192,7 @@ else:
     else:
         thinw = int(args.thin_walk[0])
 
+# Dictionary for renaming variables
 dvn = {}
 if args.dict_var_names is not None:
     with open(args.dict_var_names, 'r') as f:
@@ -237,6 +206,7 @@ if args.dict_var_names is not None:
         elif spline != []:
             raise ValueError("Formatting error in variable names dictionary:\n%s" % line)
 
+# Dictionary for labelling variables
 dl = {}
 if args.dict_labels is not None:
     with open(args.dict_labels, 'r') as f:
@@ -250,66 +220,34 @@ if args.dict_labels is not None:
         elif spline != []:
             raise ValueError("Formatting error in labels names dictionary:\n%s" % line)
 
-####################################################################################
-####################################################################################
 
-# Read and parse chain + copy beforehand if requested
+#################################
+### Reading and parsing chain ###
+#################################
 
-input_fname = os.path.realpath(args.file[0]) # Input chain filename
-
-if input_fname.endswith('.h5'): # Check if HDF5 file
-    import emcee
-
-if args.copy is not None: # Do the copy if requested
-    ran = str(np.random.rand())[2:]
-    fname = "%s/%s" % (cp_path, ran)
-    print("Copying %s into %s" % (input_fname, fname))
-    os.system('cp -f %s %s' % (input_fname, fname))
-else:
-    fname = input_fname
+fname = os.path.realpath(args.file[0]) # Input chain filename
 
 # Read chain content
-ini_fname_nosuffix = input_fname.replace(".h5", "").replace(".txt", "")
+ini_fname_nosuffix = fname.replace(".txt", "")
 ini_fname = ini_fname_nosuffix + '.ini'
 ini = parse_ini_file(ini_fname, silent_mode=True)
-par_names = np.array(
-    [dvn[par[1]] if par[1] in dvn.keys() else par[1] for par in ini['var_par']]
-)
-par_labels = np.array(
-    [dl[pn] if pn in dl.keys() else pn for pn in par_names]
-)
-if ftype == "HDF5":
-    reader = emcee.backends.HDFBackend(fname, read_only=True)
-    ln = reader.get_log_prob()
-    ch = reader.get_chain()
-    bl = reader.get_blobs()
-    blobs_names = np.array(
-        [dvn[n] if n in dvn.keys() else n for n in list(bl.dtype.names)]
-    )
-    blobs_labels = np.array(
-        [dl[n] if n in dl.keys() else n for n in blobs_names]
-    )
-    n_blobs = len(blobs_names)
-    n_steps, n_walkers, n_par = ch.shape
-    bl = bl.view(dtype=np.float64).reshape(n_steps, n_walkers, -1)
-else:
-    n_par = len(ini['var_par'])
-    n_walkers = ini['n_walkers']
-    with open(fname, 'r') as f:
-        header = f.readline()
-    all_names = [s.split(':')[1] for s in header[1:].split()]
-    n_blobs = len(all_names) - n_par - 2
-    blobs_names = np.array(
-        [dvn[n] if n in dvn.keys() else n for n in all_names[-n_blobs:]]
-    )
-    blobs_labels = np.array(
-        [dl[n] if n in dl.keys() else n for n in blobs_names]
-    )
-    tmp = np.loadtxt(fname)#.reshape(-1, n_walkers, len(all_names))
-    ln = tmp[:, 1].reshape(-1, n_walkers)
-    ch = tmp[:, 2:2+n_par].reshape(-1, n_walkers, n_par)
-    bl = tmp[:, 2+n_par:].reshape(-1, n_walkers, n_blobs)
-    n_steps = ln.shape[0]
+par_names = np.array([dvn[par[1]] if par[1] in dvn.keys() else par[1]
+                      for par in ini['var_par']])
+par_labels = np.array([dl[pn] if pn in dl.keys() else pn for pn in par_names])
+n_par = len(ini['var_par'])
+n_walkers = ini['n_walkers']
+with open(fname, 'r') as f:
+    header = f.readline()
+all_names = [s.split(':')[1] for s in header[1:].split()]
+n_blobs = len(all_names) - n_par - 2
+blobs_names = np.array([dvn[n] if n in dvn.keys() else n
+                        for n in all_names[-n_blobs:]])
+blobs_labels = np.array([dl[n] if n in dl.keys() else n for n in blobs_names])
+tmp = np.loadtxt(fname)#.reshape(-1, n_walkers, len(all_names))
+ln = tmp[:, 1].reshape(-1, n_walkers)
+ch = tmp[:, 2:2+n_par].reshape(-1, n_walkers, n_par)
+bl = tmp[:, 2+n_par:].reshape(-1, n_walkers, n_blobs)
+n_steps = ln.shape[0]
 
 # Read priors
 lbs = np.array([par[3] for par in ini['var_par']])
@@ -366,12 +304,7 @@ if 'temperature' in blobs_names:
 else:
     temp = np.ones(ln.shape)
 
-
-####################################################################################
-####################################################################################
-
 # If requested, make getdist-formatted version of the chain
-# if args.output_getdist is not None:
 if args.output_getdist:
     from getdist.mcsamples import MCSamples
     gdist = MCSamples(
@@ -384,8 +317,9 @@ if args.output_getdist:
     gdist.saveAsText(ini_fname_nosuffix + '_gdist')
 
 
-####################################################################################
-####################################################################################
+#####################
+### Produce plots ###
+#####################
 
 if 1 in plot:
     fig1, ax1 = plt.subplots(1, 1)
@@ -542,10 +476,6 @@ if 5 in plot:
     ax5.plot(x, np.poly1d(np.polyfit(x,y,3))(x), color='black', lw=2)
     ax5.set_xlabel("MCMC step")
     ax5.set_ylabel("Mean acceptance rate")
-
-if args.copy is not None:
-    print("Cleaning up temporary file %s" % fname)
-    os.system('rm %s' % fname)
 
 if len(plot) > 0:
     plt.show()
